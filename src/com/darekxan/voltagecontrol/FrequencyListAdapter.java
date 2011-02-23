2b76909e915398dafdc47d56bc6d3fd058a2e6db
darek.xan <derek.xan@gmail.com>
2011-02-23 12:08:17 +0000

    3.0.1 (bugs, bugs...)

--- src/com/darekxan/voltagecontrol/FrequencyListAdapter.java
+++ src/com/darekxan/voltagecontrol/FrequencyListAdapter.java
@@ -1,8 +1,6 @@
 package com.darekxan.voltagecontrol;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 import android.content.Context;
 import android.view.LayoutInflater;
@@ -14,12 +12,17 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class FrequencyListAdapter extends BaseExpandableListAdapter {
-	protected static final String 	C_FREQUENCY_VOLTAGE_TABLE = "cat /sys/devices/system/cpu/cpu0/cpufreq/frequency_voltage_table";
+	/**
+	 * 
+	 */
+	private final VoltageControl voltageControl;
+
 	private Context context;
-	public static Map<String, String> stock_voltages = new HashMap<String, String>();
+
 	ArrayList<ProcessorFrequency> mFqList = new ArrayList<ProcessorFrequency>();
-	
-	public FrequencyListAdapter(Context context) {
+
+	public FrequencyListAdapter(VoltageControl voltageControl, Context context) {
+		this.voltageControl = voltageControl;
 		this.context = context;
 	}
 	
@@ -43,7 +46,7 @@ public class FrequencyListAdapter extends BaseExpandableListAdapter {
 			boolean isLastChild, View convertView, ViewGroup parent) {
 
 		LinearLayout moduleLayout = (LinearLayout) LayoutInflater.from(
-				context).inflate(R.layout.module, parent, false);
+				this.voltageControl.getBaseContext()).inflate(R.layout.module, parent, false);
 		final SeekBar seekBar;
 		final TextView progressText;
 		seekBar = (SeekBar) moduleLayout.findViewById(R.id.freqSB);
@@ -84,7 +87,7 @@ public class FrequencyListAdapter extends BaseExpandableListAdapter {
 		String frequency_s = String.valueOf(frequency);
 		String stock_voltage;
 		String actual_voltage;
-		stock_voltage = stock_voltages.get(String.valueOf(frequency));
+		stock_voltage = this.voltageControl.stock_voltages.get(String.valueOf(frequency));
 		String uv = Integer.toString(mFqList.get(groupPosition).getUv());
 		if (stock_voltage != null)
 			actual_voltage = String.valueOf((Integer
@@ -136,46 +139,5 @@ public class FrequencyListAdapter extends BaseExpandableListAdapter {
 
 	public void setFrequencies(ArrayList<ProcessorFrequency> mFqList) {
 		this.mFqList = mFqList;
-	}
-	public static void getfrequency_voltage_table() {
-		String frequency_voltage_table = ShellInterface
-				.getProcessOutput(C_FREQUENCY_VOLTAGE_TABLE);
-		if (frequency_voltage_table.equals("")) {
-			stock_voltages.put("100", "950");
-			stock_voltages.put("200", "950");
-			stock_voltages.put("400", "1050");
-			stock_voltages.put("800", "1200");
-			stock_voltages.put("1000", "1275");
-			stock_voltages.put("1120", "1300");
-			stock_voltages.put("1200", "1300");
-		} else {
-			String[] tfrequency_voltage_table = frequency_voltage_table
-					.split(" ");
-			String[] frequency_table = new String[20];
-			String[] voltage_table = new String[20];
-			int j = 0;
-			// Toast.makeText(getApplicationContext(),
-			// tfrequency_voltage_table[0],
-			// Toast.LENGTH_LONG).show();
-			for (int i = 0; i < tfrequency_voltage_table.length; i += 3) {
-
-				frequency_table[j] = String
-						.valueOf(tfrequency_voltage_table[i]);
-				voltage_table[j] = String
-						.valueOf(tfrequency_voltage_table[i + 1]);
-				stock_voltages.put(
-						String.valueOf(tfrequency_voltage_table[i]
-								.subSequence(
-										0,
-										tfrequency_voltage_table[i]
-												.length() - 3)),
-						String.valueOf(tfrequency_voltage_table[i + 1]));
-				// Log.d("VC", frequency_table[j]+ ": "+
-				// voltage_table[j]);
-				j++;
-			}
-			// stock_voltages.put("100", "950");
-
-		}
 	}
 }
